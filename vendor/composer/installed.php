2a89d76cc1fbc4f0d5457e0809d17a5d8c988757
NicoHood <git@nicohood.de>
2020-12-21 17:52:45 +0100

    Added autoloader via compose update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c2056a6007cf469480b7d0a119a5ff6a173dd3f3',
+    'name' => 'nicohood/matomo',
+  ),
+  'versions' => 
+  array (
+    'nicohood/matomo' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c2056a6007cf469480b7d0a119a5ff6a173dd3f3',
+    ),
+  ),
+);
